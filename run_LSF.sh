dac29fd503b2e694650929a00a0590dbbe7bb140
Tomas Pluskal <plusik@gmail.com>
2019-02-18 15:44:17 -0500

    Fixed bsub logging

--- run_LSF.sh
+++ run_LSF.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-snakemake --latency-wait 60 --jobs 10000 --cluster-sync 'bsub -K -n {threads} -R "rusage[mem={params.memory}000] span[hosts=1]" -oo bsub_{log}' "$@"
+snakemake --latency-wait 60 --jobs 10000 --cluster-sync 'bsub -K -n {threads} -R "rusage[mem={params.memory}000] span[hosts=1]" -oo {log}.bsub' "$@"
 
