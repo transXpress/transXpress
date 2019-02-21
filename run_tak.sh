6775a96b03d141de4c314365def6bcb213b32665
Tomas Pluskal <plusik@gmail.com>
2019-02-21 13:55:47 -0500

    Updated NCBI blast for tak-specific script

--- run_tak.sh
+++ run_tak.sh
@@ -8,7 +8,7 @@ export PATH=/lab/solexa_weng/testtube/transrate-1.0.3-linux-x86_64:$PATH
 export PATH=/lab/solexa_weng/testtube/signalp-4.1:$PATH
 export PATH=/lab/solexa_weng/testtube/targetp-1.1:$PATH
 export PATH=/lab/solexa_weng/testtube/tmhmm-2.0c/bin:$PATH
-export PATH=/lab/solexa_weng/testtube/ncbi-blast-2.7.1+/bin:$PATH
+export PATH=/lab/solexa_weng/testtube/ncbi-blast-2.8.1+/bin:$PATH
 export PATH=/lab/solexa_weng/testtube/interproscan:$PATH
 export PATH=/lab/solexa_weng/testtube/miniconda3/bin/:$PATH
 
