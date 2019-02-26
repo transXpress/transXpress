7971d817f38628931417f2a87fb01a2f5b39a51e
Tomas Pluskal <plusik@gmail.com>
2019-02-25 23:51:10 -0500

    Removed reports

--- transXpress.sh
+++ transXpress.sh
@@ -2,9 +2,9 @@
 
 echo "Running the transXpress pipeline using snakemake"
 if [ ! -z `which bsub` ]; then
-  snakemake --latency-wait 60 --jobs 10000 --cluster "bsub -oo {log}.bsub -n {threads} -R rusage[mem={params.memory}000] -R span[hosts=1]" --report snakemake_report.html "$@"
+  snakemake --latency-wait 60 --jobs 10000 --cluster "bsub -oo {log}.bsub -n {threads} -R rusage[mem={params.memory}000] -R span[hosts=1]" "$@"
 else
-  snakemake --cores 8 --report snakemake_report.html "$@"
+  snakemake --cores 8 "$@"
 fi
 
 
