6f6f515a3597018d4fa1b63f9426c909891ef805
Tomas Pluskal <plusik@gmail.com>
2019-04-29 13:46:57 -0400

    Added slurm support

--- transXpress.sh
+++ transXpress.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
 echo "Running the transXpress pipeline using snakemake"
+
+if [ ! -z `which sbatch` ]; then
+  echo "Submitting snakemake jobs to SLURM cluster"
+  snakemake --latency-wait 60 --restart-times 1 --jobs 10000 --cluster "sbatch -o {log}.slurm.out -e {log}.slurm.err -n {threads} --mem {params.memory}GB" "$@"
+else
 if [ ! -z `which bsub` ]; then
-  snakemake --latency-wait 60 --jobs 10000 --cluster "bsub -oo {log}.bsub -n {threads} -R rusage[mem={params.memory}000] -R span[hosts=1]" "$@"
+  echo "Submitting snakemake jobs to LSF cluster"
+  snakemake --latency-wait 60 --restart-times 1 --jobs 10000 --cluster "bsub -oo {log}.bsub -n {threads} -R rusage[mem={params.memory}000] -R span[hosts=1]" "$@"
 else
   snakemake --cores 8 "$@"
 fi
