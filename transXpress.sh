c3e4ccb0fd6b9077f3336ee17fd257451a12d4ab
Tomas Pluskal <plusik@gmail.com>
2019-07-01 10:56:15 -0400

    Fixed a bug in the script

--- transXpress.sh
+++ transXpress.sh
@@ -2,15 +2,28 @@
 
 echo "Running the transXpress pipeline using snakemake"
 
+CLUSTER="NONE"
+
 if [ ! -z `which sbatch` ]; then
-  echo "Submitting snakemake jobs to SLURM cluster"
-  snakemake --latency-wait 60 --restart-times 1 --jobs 10000 --cluster "sbatch -o {log}.slurm.out -e {log}.slurm.err -n {threads} --mem {params.memory}GB" "$@"
-else
+  CLUSTER="SLURM"
+fi
+
 if [ ! -z `which bsub` ]; then
+  CLUSTER="LSF"
+fi
+
+case "$CLUSTER" in
+"LSF")
   echo "Submitting snakemake jobs to LSF cluster"
   snakemake --latency-wait 60 --restart-times 1 --jobs 10000 --cluster "bsub -oo {log}.bsub -n {threads} -R rusage[mem={params.memory}000] -R span[hosts=1]" "$@"
-else
+  ;;
+"SLURM")
+  echo "Submitting snakemake jobs to SLURM cluster"
+  snakemake --latency-wait 60 --restart-times 1 --jobs 10000 --cluster "sbatch -o {log}.slurm.out -e {log}.slurm.err -n {threads} --mem {params.memory}GB" "$@"
+  ;;
+*)
   snakemake --cores 8 "$@"
-fi
+  ;;
+esac
 
 
